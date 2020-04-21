14f86ef69c10ef7afaae54f19aca509bbee5a238
Igor Novikov <novikov9@yahoo.com>
2020-04-21 11:44:06 +0300

    save point

---
+++ src/advanced_classes/Challenge.kt
@@ -0,0 +1,6 @@
+package advanced_classes
+
+fun main(args:Array<String>){
+
+}
+
---
+++ src/advanced_classes/Test.kt
@@ -0,0 +1,39 @@
+package advanced_classes
+
+fun main(args:Array<String>){
+    val ref = C()
+    //При приведении типов получаем доступ к уникальным методам класса
+    (ref as A).aMethod()
+    //Но при вызове переопределенных методов вызовется
+    (ref as A).method()
+}
+
+open class A{
+    open fun method(){
+        println("Method from class - A")
+    }
+
+    open fun aMethod(){
+        println("Unique method from class - A ")
+    }
+}
+
+open class B :A(){
+    override fun method() {
+        println("Method from class - B")
+    }
+
+    open fun bMethod(){
+        println("Unique method from class - B ")
+    }
+}
+
+open class C : B(){
+    override fun method(){
+        println("Method from class - C")
+    }
+
+    open fun cMethod(){
+        println("Unique method from class - C ")
+    }
+}
